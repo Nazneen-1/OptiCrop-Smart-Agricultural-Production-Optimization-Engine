5b8ddc6e9d629237ca6140208099da8d5b45afdd
Nazneen-1 <nazneenfirdous741@gmail.com>
2026-07-08 12:11:50 +0530

    Prepare project for deployment

--- data_collection_analysis/app.py
+++ data_collection_analysis/app.py
@@ -58,5 +58,10 @@ def predict():
         return render_template('findyourcrop.html', 
                              prediction_text='Error in prediction: {}'.format(str(e)))
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
